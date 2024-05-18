6b5d5fc5a0bb684967194bdadb9b05a0ac55bb21
Nitya <nityas727@gmail.com>
2024-05-18 19:12:40 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+const { db } = require('./db/db')
+const {readdirSync} = require('fs')
+const app = express()
+
+// import {db} from './db/db'
+
+require('dotenv').config()
+
+const PORT  =process.env.PORT
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//routes
+readdirSync('./routes').map((route) => app.use('/api', require('./routes/' + route)))
+
+
+const server = () =>{
+    db()
+    app.listen(PORT, ()=>{
+        console.log('listening to port:', PORT)
+    })
+}
+
+server()
